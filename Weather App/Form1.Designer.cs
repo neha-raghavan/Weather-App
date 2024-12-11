09e5d43b53431bd267659f516114a3cecf84211b
rpneh <rpneh@NEHA-WINDOWS>
2024-12-11 18:04:09 +0400

    Add project files.

---
+++ Weather App/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿using System.Windows.Forms;
+
+namespace Weather_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblWeather = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtSearch = new Guna.UI2.WinForms.Guna2TextBox();
+            this.pbWeather = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lblWeather, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblTemp, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 409);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.69565F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.30435F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(451, 148);
+            this.tableLayoutPanel1.TabIndex = 6;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // lblWeather
+            // 
+            this.lblWeather.AutoSize = true;
+            this.lblWeather.BackColor = System.Drawing.Color.Transparent;
+            this.lblWeather.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeather.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblWeather.Location = new System.Drawing.Point(3, 72);
+            this.lblWeather.Name = "lblWeather";
+            this.lblWeather.Size = new System.Drawing.Size(445, 76);
+            this.lblWeather.TabIndex = 4;
+            this.lblWeather.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTemp.BackColor = System.Drawing.Color.Transparent;
+            this.lblTemp.Font = new System.Drawing.Font("Segoe UI Light", 29F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.ForeColor = System.Drawing.Color.Lime;
+            this.lblTemp.Location = new System.Drawing.Point(3, 0);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(445, 72);
+            this.lblTemp.TabIndex = 5;
+            this.lblTemp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTemp.Click += new System.EventHandler(this.lblTemp_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoScroll = true;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.txtSearch, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.pbWeather, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(56, 12);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.76471F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.18182F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.84848F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(457, 660);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(3, 563);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 0);
+            this.panel1.TabIndex = 8;
+            this.panel1.WrapContents = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderRadius = 6;
+            this.txtSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSearch.DefaultText = "";
+            this.txtSearch.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtSearch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtSearch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSearch.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSearch.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtSearch.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtSearch.Location = new System.Drawing.Point(2, 2);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PasswordChar = '\0';
+            this.txtSearch.PlaceholderText = "";
+            this.txtSearch.SelectedText = "";
+            this.txtSearch.Size = new System.Drawing.Size(453, 73);
+            this.txtSearch.TabIndex = 6;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearch_KeyPress);
+            // 
+            // pbWeather
+            // 
+            this.pbWeather.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbWeather.Location = new System.Drawing.Point(3, 167);
+            this.pbWeather.Name = "pbWeather";
+            this.pbWeather.Size = new System.Drawing.Size(451, 236);
+            this.pbWeather.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbWeather.TabIndex = 2;
+            this.pbWeather.TabStop = false;
+            this.pbWeather.Click += new System.EventHandler(this.pbWeather_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(3, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(451, 81);
+            this.panel2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(556, 716);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weather";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWeather)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private Guna.UI2.WinForms.Guna2TextBox txtSearch;
+        private System.Windows.Forms.Label lblWeather;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.FlowLayoutPanel panel1;
+        private PictureBox pbWeather;
+        private FlowLayoutPanel panel2;
+    }
+}
+
---
+++ Weather App/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Guna.UI2.WinForms;
+
+namespace Weather_App
+{
+    public partial class Form1 : Form
+    {
+        private const string ApiKey = "073bd01b8b193f3dfc7db66c5a7a8c94";
+        private List<string> recentSearches = new List<string>(); 
+        private const string filePath = "recent_searches.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadRecentSearches();
+            //txtSearch.Click += txtSearch_KeyPress;
+            PictureBox pbWeather = new PictureBox
+            {
+               
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Location = new Point(10, 10)
+                
+            };
+            Label lblTemp = new Label
+            {
+              
+                ForeColor = Color.White,
+                BackColor = Color.Transparent, // Ensure the label background is transparent
+                AutoSize = true,
+                Location = new Point(50, 50) // Position relative to the PictureBox
+            };
+
+            Label lblWeather = new Label
+            {
+               
+                ForeColor = Color.White,
+                BackColor = Color.Transparent,
+                AutoSize = true,
+                Location = new Point(150, 100) 
+            };
+            lblTemp.Parent = pbWeather;
+            lblWeather.Parent = pbWeather;
+            pbWeather.Controls.Add(lblTemp);
+            this.Controls.Add(pbWeather);
+        }
+        private void LoadRecentSearches()
+        {
+            if (File.Exists(filePath))
+            {
+                recentSearches = new List<string>(File.ReadAllLines(filePath));
+            }
+        }
+        private void SaveRecentSearches()
+        {
+          
+
+               
+                File.WriteAllLines(filePath, recentSearches);
+                
+            
+        }
+      
+        private void UpdateWeatherImage(string weatherCondition)
+    {
+        // Set the appropriate image based on the weather condition
+        switch (weatherCondition.ToLower())
+        {
+            case "clear":
+                pbWeather.Image = Properties.Resources.Summer; // Example: sunny image
+                break;
+            case "rain":
+                    pbWeather.Image = Properties.Resources.Rainy; // Example: rainy image
+                break;
+            case "clouds":
+                    pbWeather.Image = Properties.Resources.Cloud; // Example: cloudy image
+                break;
+            case "snow":
+                    pbWeather.Image = Properties.Resources.snow; // Example: snowy image
+                break;
+            default:
+                    pbWeather.Image = Properties.Resources.temp; // Default image
+                break;
+        }
+    }
+   
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+      
+
+ 
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSearch.Text = "Enter city...";
+            txtSearch.ForeColor = Color.Gray;
+
+            txtSearch.GotFocus += RemovePlaceholder;
+            txtSearch.LostFocus += SetPlaceholder;
+            DisplayRecentSearchButtons();
+        }
+        private void DisplayRecentSearchButtons()
+        {
+
+            panel2.Controls.Clear();
+
+            // Loop through the recentSearches and display them
+            for (int i = recentSearches.Count - 1; i >= 0; i--)
+            {
+             
+            string search = recentSearches[i];
+
+                // Create a new Guna2Chip for each search term
+                Guna2Chip searchChip = new Guna2Chip
+                {
+                    Text = search,
+                    Width = 150,
+                    Height = 25,
+                    Margin = new Padding(5),
+                    Tag = search // Store the search term in the Tag property
+                };
+
+                // Add click event to perform a search when the chip is clicked
+                searchChip.Click += (s, e) =>
+                {
+                    txtSearch.Text = (string)((Guna2Chip)s).Tag; // Set the chip's text in the search box
+                    SearchWeather((string)((Guna2Chip)s).Tag);  // Perform the search
+                };
+
+                // Add the chip to the panel
+                panel2.Controls.Add(searchChip);
+            }
+
+
+        }
+        private void RemovePlaceholder(object sender, EventArgs e)
+        {
+            Guna.UI2.WinForms.Guna2TextBox textBox = (Guna.UI2.WinForms.Guna2TextBox)sender;
+            if (textBox.Text == "Enter city...")
+            {
+                textBox.Text = "";
+                textBox.ForeColor = Color.Black; // Default color
+            }
+        }
+
+        private void SetPlaceholder(object sender, EventArgs e)
+        {
+            Guna.UI2.WinForms.Guna2TextBox textBox = (Guna.UI2.WinForms.Guna2TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Enter city...";
+                textBox.ForeColor = Color.Gray;
+            }
+        }
+
+        private async void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string city = txtSearch.Text.Trim();
+                if (string.IsNullOrEmpty(city))
+                {
+                    MessageBox.Show("Please Enter a city name", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                SearchWeather(city);
+            }
+        }
+
+        private async void SearchWeather(string city)
+        {
+            string ApiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={ApiKey}&units=metric";
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(ApiUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        JObject weatherData = JObject.Parse(responseBody);
+
+                        double temperature = weatherData["main"]["temp"].ToObject<double>();
+                        string description = weatherData["weather"][0]["description"].ToString();
+                        string weatherCondition = (string)weatherData["weather"][0]["main"];
+                        lblTemp.Text = $"{temperature} °C";
+                        lblWeather.Text = description;
+                        UpdateWeatherImage(weatherCondition);
+                        if (recentSearches.Contains(city))
+                        {
+                            recentSearches.Remove(city);
+                        }
+                        recentSearches.Insert(0, city);
+
+                        if (recentSearches.Count > 3)
+                        {
+                            recentSearches.RemoveAt(recentSearches.Count - 1);
+                        }
+                        DisplayRecentSearchButtons();
+                    }
+                    else
+                    {
+                        lblWeather.Text = "Invalid city";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblTemp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveRecentSearches();
+        }
+
+        private void pbWeather_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbWeather_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Chip1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
